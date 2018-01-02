78a149f4e0f6aca451b5ca8cc59f5533e5fbd907
Lukas Camargo (Americas) <lukas.camargo@dimensiondata.com>
2018-01-02 17:17:59 -0200

    Merge complete

--- server/config/database/databaseConfig.js
+++ server/config/database/databaseConfig.js
@@ -1,46 +1,25 @@
-module.exports = function(uri) {
+module.exports = function (uri) {
     let mongoose = require('mongoose');
-<<<<<<< HEAD
-=======
     mongoose.Promise = global.Promise;
->>>>>>> 462de9f4c9b5087f19df475bd5ffa19bd84d02e4
 
     mongoose.connect(uri);
 
-    mongoose.connection.on('connected', function() {
-<<<<<<< HEAD
-        console.log('Conectado ao MongoDB')
-    })
-
-    mongoose.connection.on('error', function(error) {
-        console.log('Erro na conexão: ' + error);
-    })
-
-    mongoose.connection.on('disconnected', function() {
-        console.log('Desconectado do MongoDB');
-    })
-=======
+    mongoose.connection.on('connected', function () {
         console.log('Connected on MongoDB');
     });
 
-    mongoose.connection.on('error', function(error) {
+    mongoose.connection.on('error', function (error) {
         console.log('Error on connection: ' + error);
     });
 
-    mongoose.connection.on('disconnected', function() {
+    mongoose.connection.on('disconnected', function () {
         console.log('Disconnected on MongoDB');
     });
->>>>>>> 462de9f4c9b5087f19df475bd5ffa19bd84d02e4
 
-    process.on('SIGNIT', function() {
-        mongoose.connection.close(function() {
+    process.on('SIGNIT', function () {
+        mongoose.connection.close(function () {
             console.log('Aplicação terminada, conexão fechada');
             process.exit(0);
         })
-<<<<<<< HEAD
-    })
-}
-=======
     });
 };
->>>>>>> 462de9f4c9b5087f19df475bd5ffa19bd84d02e4
