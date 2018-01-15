ec070e4fe483c5c21d5faf7b35922c6411849065
Guilherme Assis <guiga_cfc@hotmail.com>
2018-01-14 23:21:09 -0200

    Test in Heroku Server

--- srcWeb/js/app/helpers/defineHost.js
+++ srcWeb/js/app/helpers/defineHost.js
@@ -2,6 +2,6 @@ angular
     .module('core')
     .service('defineHost', function () {
         return {
-            host : 'https://rhinozapp.herokuapp.com/:80'
+            host : 'https://rhinozapp.herokuapp.com/'
         };
     });
